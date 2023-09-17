3f2d0628e5257d292dd5609e9774ace3097e9215
PutriDeylin <putrideylin82@gmail.com>
2023-09-17 11:41:55 +0700

    [DOCS] penambahan file html dan js

---
+++ assets/js/scriptproduct.js
@@ -0,0 +1,121 @@
+// Menyimpan data produk dalam bentuk array
+const products = [
+    {
+        name: 'VIVO Y22',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp1.980.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/vivo1.jpg',
+        link: '#'
+    },
+    {
+        name: 'VIVO Y16',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp1.494.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/vivo2.png',
+        link: '#'
+    },
+    {
+        name: 'VIVO Y17',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp824.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9734;',
+        imageSrc: '../assets/image/vivo3.jpg',
+        link: '#'
+    },
+    {
+        name: 'VIVO 20s',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp1.110.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9734;',
+        imageSrc: '../assets/image/vivo4.jpg',
+        link: '#'
+    },
+    {
+        name: 'OPPO A57',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp2.399.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/oppo1.jpg',
+        link: '#'
+    }, 
+    {
+        name: 'OPPO A78',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp3.999.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/oppo2.jpg',
+        link: '#'
+    }, 
+    {
+        name: 'Samsung A34',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp5.399.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/samsung1.jpg',
+        link: '#'
+    }, 
+    {
+        name: 'Samsung Z Flip 5',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp17.999.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/samsung2.jpg',
+        link: '#'
+    }, 
+    {
+        name: 'Xiomi Note 11 Pro',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp3.999.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/xiomi1.jpg',
+        link: '#'
+    }, 
+    {
+        name: 'Xiomi Note 10',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp2.549.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/xiomi2.jpg',
+        link: '#'
+    }, 
+    {
+        name: 'Realme X3',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp6.100.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/realme1.jpg',
+        link: '#'
+    }, 
+    {
+        name: 'Realme Narzo 30A',
+        description: 'HP VIVO adalah smartphone canggih dengan berbagai fitur terbaru.',
+        price: 'Rp2.499.000',
+        rating: '&#9733; &#9733; &#9733; &#9733; &#9733;',
+        imageSrc: '../assets/image/realme2.jpg',
+        link: '#'
+    }, 
+];
+
+// Menambahkan produk ke dalam galeri produk
+const productGallery = document.getElementById('product-gallery');
+products.forEach(product => {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    productCard.innerHTML = `
+        <img src="${product.imageSrc}" alt="${product.name}">
+        <h3>${product.name}</h3>
+        <p>${product.description}</p>
+        <p>Price: ${product.price}</p>
+        <button class="beli-btn">Buy</button>
+        <div class="rating">${product.rating}</div>
+        <a href="${product.link}">Selengkapnya</a>
+    `;
+
+    productGallery.appendChild(productCard);
+});
+
+
+
